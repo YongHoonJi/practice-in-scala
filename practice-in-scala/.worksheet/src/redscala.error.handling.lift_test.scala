9fea05b0c6319b149bbbfccfa9752ca7ab46676a
YongHoonJi <Justin@218.38.137.28>
2017-08-30 00:09:11 +0900

    add

--- practice-in-scala/.worksheet/src/redscala.error.handling.lift_test.scala
+++ practice-in-scala/.worksheet/src/redscala.error.handling.lift_test.scala
@@ -1,8 +1,16 @@
 package redscala.error.handling
 
-object lift_test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(95); 
-  println("Welcome to the Scala worksheet");$skip(104); 
-
+object lift_test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(111); 
+	def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f;System.out.println("""lift: [A, B](f: A => B)redscala.error.handling.Option[A] => redscala.error.handling.Option[B]""");$skip(54); 
+	val abs1:Option[Int] => Option[Int] = lift(math.abs);System.out.println("""abs1  : redscala.error.handling.Option[Int] => redscala.error.handling.Option[Int] = """ + $show(abs1 ));$skip(61); 
+	val abs0: Option[Double] => Option[Double] = lift(math.abs);System.out.println("""abs0  : redscala.error.handling.Option[Double] => redscala.error.handling.Option[Double] = """ + $show(abs0 ));$skip(14); val res$0 = 
+	math.abs(-1);System.out.println("""res0: Int = """ + $show(res$0));$skip(18); val res$1 = 
+  abs0(Some(1.0));System.out.println("""res1: redscala.error.handling.Option[Double] = """ + $show(res$1));$skip(23); 
+  
+  val sx = Some(-1);System.out.println("""sx  : redscala.error.handling.Some[Int] = """ + $show(sx ));$skip(19); val res$2 = 
+  sx.map(math.abs);System.out.println("""res2: redscala.error.handling.Option[Int] = """ + $show(res$2));$skip(16); val res$3 = 
+	abs1(Some(-1));System.out.println("""res3: redscala.error.handling.Option[Int] = """ + $show(res$3));$skip(105); 
+	
   def Try[A](a: => A): Option[A] = {
       try Some(a)
       catch { case e: Exception => None }
@@ -26,8 +34,8 @@ object lift_test {;import org.scalaide.worksheet.runtime.library.WorksheetSuppor
 			
 		// 두 option 값 중 하나라도 None이면 결과 역시 None
     def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-      a flatMap (aa => b map (bb => f(aa, bb)));System.out.println("""map2: [A, B, C](a: redscala.error.handling.Option[A], b: redscala.error.handling.Option[B])(f: (A, B) => C)redscala.error.handling.Option[C]""");$skip(87); val res$0 = 
+      a flatMap (aa => b map (bb => f(aa, bb)));System.out.println("""map2: [A, B, C](a: redscala.error.handling.Option[A], b: redscala.error.handling.Option[B])(f: (A, B) => C)redscala.error.handling.Option[C]""");$skip(87); val res$4 = 
                                                   
-		parseInsuranceRateQuote("2", "3");System.out.println("""res0: redscala.error.handling.Option[Double] = """ + $show(res$0))}
+		parseInsuranceRateQuote("2", "3");System.out.println("""res4: redscala.error.handling.Option[Double] = """ + $show(res$4))}
       
 }
--- practice-in-scala/src/redscala/error/handling/lift_test.sc
+++ practice-in-scala/src/redscala/error/handling/lift_test.sc
@@ -1,8 +1,22 @@
 package redscala.error.handling
 
 object lift_test {
-  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
-
+	def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
+                                                  //> lift: [A, B](f: A => B)redscala.error.handling.Option[A] => redscala.error.h
+                                                  //| andling.Option[B]
+	val abs1:Option[Int] => Option[Int] = lift(math.abs)
+                                                  //> abs1  : redscala.error.handling.Option[Int] => redscala.error.handling.Optio
+                                                  //| n[Int] = <function1>
+	val abs0: Option[Double] => Option[Double] = lift(math.abs)
+                                                  //> abs0  : redscala.error.handling.Option[Double] => redscala.error.handling.Op
+                                                  //| tion[Double] = <function1>
+	math.abs(-1)                              //> res0: Int = 1
+  abs0(Some(1.0))                                 //> res1: redscala.error.handling.Option[Double] = Some(1.0)
+  
+  val sx = Some(-1)                               //> sx  : redscala.error.handling.Some[Int] = Some(-1)
+  sx.map(math.abs)                                //> res2: redscala.error.handling.Option[Int] = Some(1)
+	abs1(Some(-1))                            //> res3: redscala.error.handling.Option[Int] = Some(1)
+	
   def Try[A](a: => A): Option[A] = {
       try Some(a)
       catch { case e: Exception => None }
@@ -29,9 +43,9 @@ object lift_test {
 			
 		// 두 option 값 중 하나라도 None이면 결과 역시 None
     def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-      a flatMap (aa => b map (bb => f(aa, bb)))   //> map2: [A, B, C](a: redscala.error.handling.Option[A], b: redscala.error.hand
-                                                  //| ling.Option[B])(f: (A, B) => C)redscala.error.handling.Option[C]
+      a flatMap (aa => b map (bb => f(aa, bb)))   //> map2: [A, B, C](a: redscala.error.handling.Option[A], b: redscala.error.han
+                                                  //| dling.Option[B])(f: (A, B) => C)redscala.error.handling.Option[C]
                                                   
-		parseInsuranceRateQuote("2", "3") //> res0: redscala.error.handling.Option[Double] = Some(6.0)
+		parseInsuranceRateQuote("2", "3") //> res4: redscala.error.handling.Option[Double] = Some(6.0)
       
 }
